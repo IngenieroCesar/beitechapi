7e01320c2f590b3164df83087e59d9e29ca09a5d
Cesar Garzon <ceagarzonpu@unal.edu.co>
2020-04-19 17:49:05 -0500

    Desarrollo: Consulta con datos de la orden para cada usuario terminada

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -18,29 +18,25 @@ class OrderController extends Controller
     }
     
     public function show( Request $request ){
- 
-        $orders= DB::table('orders')->select('orders.id',
-                 DB::raw("(GROUP_CONCAT(order_details.quantity SEPARATOR '-')) as `Cantidad`"),
-                 DB::raw("(GROUP_CONCAT(products.name SEPARATOR '-')) as `Porducts`") )
+        
+        //Consulta para listar ordenes de compra de cada usuario
+        $orders= DB::table('orders')->select('orders.created_at as creationdate',
+                    'orders.id as orderid', 'orders.total as total', 
+                    DB::raw("(GROUP_CONCAT(order_details.quantity SEPARATOR ', ')) as `quantity`"),
+                    DB::raw("(GROUP_CONCAT(products.name SEPARATOR ', ')) as `products`") )
                  ->join('order_details', 'orders.id', '=', 'order_details.order_id')
                  //Tabla para relaciónar con la orden
                  ->join('product_user', 'orders.user_id', '=', 'product_user.user_id')
                  ->join('products', 'product_user.product_id', '=', 'products.id')
                  ->whereColumn('order_details.description', 'products.description')
+                 //Condicional para el id del usuario
+                 ->where('orders.user_id', $request->id)
                  ->groupBy('orders.id' );
         $getorder = $orders->get();
-
-
-
-        $orden = DB::table('orders')->select(DB::raw("(GROUP_CONCAT(order_details.id SEPARATOR '-')) as `Cantidad`"))
-            ->join('order_details', 'orders.id', '=', 'order_details.order_id')
-            ->groupBy('orders.id' )
-            ->get();
-
 
         //Generamos un arreglo con los datos necesarios basados en el ID del cliente
 
 
-        return response()->json($orden);
+        return response()->json($getorder);
     }
 }
