b8d0a6249ae50c0b7faf01dfdb930a95848e1025
Cesar Garzon <ceagarzonpu@unal.edu.co>
2020-04-21 11:58:13 -0500

    Solucion de problema CORS, comunicacion con SPA en produccion

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -19,6 +19,8 @@ class Kernel extends HttpKernel
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
+        // \App\Http\Middleware\Cors::class,//cors added here 
+        \Fruitcake\Cors\HandleCors::class,
     ];
 
     /**
@@ -35,7 +37,7 @@ class Kernel extends HttpKernel
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
-            \Fruitcake\Cors\HandleCors::class,
+
         ],
 
         'api' => [
@@ -62,6 +64,7 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        'Cors' => \App\Http\Middleware\Cors::class,//cors added here 
     ];
 
     /**
